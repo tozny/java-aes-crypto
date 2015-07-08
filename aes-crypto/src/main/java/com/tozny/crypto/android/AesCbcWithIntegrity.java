91ce2950839302c76f455b2edb1796b19590d305
Isaac Potoczny-Jones <ijones@syntaxpolice.org>
2015-07-08 09:13:48 -0700

    Update copyright

--- aes-crypto/src/main/java/com/tozny/crypto/android/AesCbcWithIntegrity.java
+++ aes-crypto/src/main/java/com/tozny/crypto/android/AesCbcWithIntegrity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Tozny LLC
+ * Copyright (c) 2014-2015 Tozny LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
